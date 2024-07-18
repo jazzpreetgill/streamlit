ea181b0429aa7399e409128048da9ddcc34376e8
jazzpreetgill <140133292+jazzpreetgill@users.noreply.github.com>
2024-07-17 21:27:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,62 +5,8 @@ import numpy as np
 
 model = load_model('dp_insurance_charges')
 
-def predict(model, input_df):
-    predictions_df = predict_model(estimator=model, data=input_df)
-    predictions = predictions_df.iloc[0]['prediction_label']
-    #prediction = int(prediction.iloc[0]['prediction_label'])
-    return predictions
-
-def run():
-
-    from PIL import Image
-    image = Image.open('logo.png')
-    image_hospital = Image.open('hospital.jpg')
-
-    st.image(image,use_column_width=False)
-
-    add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
-    ("Online", "Batch"))
-
-    st.sidebar.info('This app is created to predict patient hospital charges')
-    st.sidebar.success('https://www.pycaret.org')
-    
-    st.sidebar.image(image_hospital)
-
-    st.title("Insurance Charges Prediction App")
-
-    if add_selectbox == 'Online':
-
-        age = st.number_input('Age', min_value=1, max_value=100, value=25)
-        sex = st.selectbox('Sex', ['male', 'female'])
-        bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-        children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
-        if st.checkbox('Smoker'):
-            smoker = 'yes'
-        else:
-            smoker = 'no'
-        region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
-
-        output=""
-
-        input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
-        input_df = pd.DataFrame([input_dict])
-
-        if st.button("Predict"):
-            output = predict(model=model, input_df=input_df)
-            output = '$' + str(output)
-
-        st.success('The output is {}'.format(output))
-
-    if add_selectbox == 'Batch':
-
-        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
-
-        if file_upload is not None:
-            data = pd.read_csv(file_upload)
-            predictions = predict_model(estimator=model,data=data)
-            st.write(predictions)
-
-if __name__ == '__main__':
-    run()
+input_dict = {'age' : 20, 'sex' : 'male', 'bmi' : 20, 'children' : 2, 'smoker' : 'yes', 'region' : 'southwest'}
+input_df = pd.DataFrame([input_dict])
+predictions_df = predict_model(estimator=model, data=input_df)
+predictions = predictions_df.iloc[0]['prediction_label']
+print(predictions)
